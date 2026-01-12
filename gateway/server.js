345f10d6f03d02b6980aad1fe7128eae74da30c9
muqeetkhan <muqeetkhan050@gmail.com>
2026-01-13 05:57:21 +1000

    added

--- gateway/server.js
+++ gateway/server.js
@@ -1,29 +1,4 @@
-// const express = require('express');
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-// const rateLimit = require('express-rate-limit');
 
-// const app = express();
-// app.set('trust proxy', 1);
-
-// // Prevent spamming the anonymous chat
-// const limiter = rateLimit({
-//   windowMs: 1 * 60 * 1000, // 1 minute
-//   max: 60, // 60 requests per minute
-//   message: "Too many messages, please slow down."
-// });
-// app.use(limiter);
-
-// // Proxy WebSocket connections to the chat-service
-// app.use('/socket.io', createProxyMiddleware({
-//   target: process.env.CHAT_SERVICE_URL,
-//   ws: true,
-//   changeOrigin: true,
-//   secure: false
-// }));
-
-// app.listen(process.env.PORT || 8080, () => {
-//   console.log('✅ Gateway running on port 8080');
-// });
 
 const express = require("express");
 const http = require("http");
@@ -50,9 +25,9 @@ const socketProxy = createProxyMiddleware({
   changeOrigin: true,
   secure: false
 });
-
-// HTTP + WS route
 app.use("/socket.io", socketProxy);
+server.on("upgrade", socketProxy.upgrade);
+
 
 // Create raw HTTP server
 const server = http.createServer(app);
